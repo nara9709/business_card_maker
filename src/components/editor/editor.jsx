194f81ac5e001327bab3ac0371cd5cb9fc0ce500
nara9709 <worldkr19@gmail.com>
2022-03-21 20:48:45 -0400

    Add Editor layout

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -5,6 +5,98 @@ const Editor = ({ cards }) => {
   return (
     <section className={styles.editor}>
       <h1 className={styles.title}>Card Maker</h1>
+      <div className={styles.inputContainer}>
+        <div className={styles.firstLineContainer}>
+          <input className={styles.firstLineInput} type="text" />
+          <input className={styles.firstLineInput} type="text" />
+          <select className={styles.section} name="Theme" id="theme">
+            <option value="1">Dark</option>
+            <option value="2">White</option>
+          </select>
+        </div>
+        <div className={styles.secondLineContainer}>
+          <input className={styles.secondLineInput} type="text" />
+          <input className={styles.secondLineInput} type="text" />
+        </div>
+        <textarea
+          className={styles.textArea}
+          name="comment"
+          id="comment"
+          cols="35"
+          rows="3"
+        ></textarea>
+        <button className={styles.imageBtn}>Image</button>
+        <button className={styles.deleteBtn}>Delete</button>
+      </div>
+      <div className={styles.inputContainer}>
+        <div className={styles.firstLineContainer}>
+          <input className={styles.firstLineInput} type="text" />
+          <input className={styles.firstLineInput} type="text" />
+          <select className={styles.section} name="Theme" id="theme">
+            <option value="1">Dark</option>
+            <option value="2">White</option>
+          </select>
+        </div>
+        <div className={styles.secondLineContainer}>
+          <input className={styles.secondLineInput} type="text" />
+          <input className={styles.secondLineInput} type="text" />
+        </div>
+        <textarea
+          className={styles.textArea}
+          name="comment"
+          id="comment"
+          cols="35"
+          rows="3"
+        ></textarea>
+        <button className={styles.imageBtn}>Image</button>
+        <button className={styles.deleteBtn}>Delete</button>
+      </div>
+      <div className={styles.inputContainer}>
+        <div className={styles.firstLineContainer}>
+          <input className={styles.firstLineInput} type="text" />
+          <input className={styles.firstLineInput} type="text" />
+          <select className={styles.section} name="Theme" id="theme">
+            <option value="1">Dark</option>
+            <option value="2">White</option>
+          </select>
+        </div>
+        <div className={styles.secondLineContainer}>
+          <input className={styles.secondLineInput} type="text" />
+          <input className={styles.secondLineInput} type="text" />
+        </div>
+        <textarea
+          className={styles.textArea}
+          name="comment"
+          id="comment"
+          cols="35"
+          rows="3"
+        ></textarea>
+        <button className={styles.imageBtn}>Image</button>
+        <button className={styles.deleteBtn}>Delete</button>
+      </div>
+      <div className={styles.inputContainer}>
+        <div className={styles.firstLineContainer}>
+          <input className={styles.firstLineInput} type="text" />
+          <input className={styles.firstLineInput} type="text" />
+          <select className={styles.section} name="Theme" id="theme">
+            <option value="1">Dark</option>
+            <option value="2">White</option>
+          </select>
+        </div>
+        <div className={styles.secondLineContainer}>
+          <input className={styles.secondLineInput} type="text" />
+          <input className={styles.secondLineInput} type="text" />
+        </div>
+        <textarea
+          className={styles.textArea}
+          name="comment"
+          id="comment"
+          cols="35"
+          rows="3"
+        ></textarea>
+        <button className={styles.imageBtn}>Image</button>
+        <button className={styles.deleteBtn}>Delete</button>
+      </div>
     </section>
   );
 };
