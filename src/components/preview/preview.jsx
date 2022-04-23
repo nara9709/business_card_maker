98c63a11deccc834edc011dd183e47d86364a316
nara9709 <worldkr19@gmail.com>
2022-04-23 19:55:52 -0400

    styling card priview

--- src/components/preview/preview.jsx
+++ src/components/preview/preview.jsx
@@ -6,9 +6,11 @@ import Card from '../card/card';
 const Preview = ({ cards }) => (
   <section className={styles.preview}>
     <h1 className={styles.title}>Card Preview</h1>
-    {cards.map((card) => (
-      <Card card={card}></Card>
-    ))}
+    <ul className={styles.cards}>
+      {cards.map((card) => (
+        <Card card={card}></Card>
+      ))}
+    </ul>
   </section>
 );
 
